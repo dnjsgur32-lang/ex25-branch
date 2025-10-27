f5cbf14ae3a058759cce41bfe367f85dca0ab893
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-10-27 16:38:51 +0900

    valid 연습

--- src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/domain/Student.java
+++ src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/domain/Student.java
@@ -1,16 +1,25 @@
 package com.ljh.ex25branch.re_mybatis_thymeleaf.domain;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Student {
-
+    
     private Long id;
+    @NotBlank(message = "이름 필수 입력 항목")
     private String name;
+
+    @Email (message = "이메일 형식 @ 입력 필수")
     private String email;
+
+    @Min(value = 1, message = "나이는 1세 이상")
     private Integer age;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
