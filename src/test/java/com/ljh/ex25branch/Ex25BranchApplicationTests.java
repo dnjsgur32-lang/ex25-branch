f67edb4e5c673182dbec5c102c5700002f098b65
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-10-27 13:57:42 +0900

    Initial commit

---
+++ src/test/java/com/ljh/ex25branch/Ex25BranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.ex25branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex25BranchApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
