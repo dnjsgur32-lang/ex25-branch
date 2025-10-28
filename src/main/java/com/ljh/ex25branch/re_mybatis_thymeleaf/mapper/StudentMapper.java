2b07afb6b8e56debac197768c48d689541619834
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-10-28 13:18:01 +0900

    mapper2

--- src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/mapper/StudentMapper.java
+++ src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/mapper/StudentMapper.java
@@ -1,7 +1,7 @@
 package com.ljh.ex25branch.re_mybatis_thymeleaf.mapper;
 
 import com.ljh.ex25branch.re_mybatis_thymeleaf.domain.Student;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -9,11 +9,18 @@ import java.util.List;
 public interface StudentMapper {
     List<Student> findAll();
 
+    @Select("SELECT * FROM student WHERE id = #{id}")
     Student findById (Long id);
 
+    @Insert("INSERT INTO student(name, email, age) \r\n"
+            + "VALUES(#{name}, #{email}, #{age})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert (Student student);
 
+    @Update("UPDATE student\r\n" + " SET NAME = #{name}, email = #{email} age = #{age}\r\n"
+            + " WHERE id = #{id}")
     void update (Student student);
 
+    @Delete("DELETE FROM student WHERE id = #{id}")
     void delete (Long id);
 }
