86c89a73db3039d71dd3b45f0e582bad1742e71d
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-10-28 13:12:41 +0900

    Create DBConnTest.java

---
+++ src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/controller/DBConnTest.java
@@ -0,0 +1,26 @@
+package com.ljh.ex25branch.re_mybatis_thymeleaf.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RestController
+public class DBConnTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/dbconn")
+    public String dbconn() {
+        try(Connection conn = dataSource.getConnection()) {
+            String result = conn.getMetaData().getURL();
+            return "DB 연결 성공: " + result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "DB 연결 실패";
+        }
+    }
+}
