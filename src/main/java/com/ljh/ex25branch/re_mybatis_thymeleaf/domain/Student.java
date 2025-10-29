07a42e7009342add0ce4b884f60018155e491f5e
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-10-29 16:27:35 +0900

    Update Student.java

--- src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/domain/Student.java
+++ src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/domain/Student.java
@@ -1,5 +1,8 @@
 package com.ljh.ex25branch.re_mybatis_thymeleaf.domain;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,8 +11,14 @@ import java.time.LocalDateTime;
 public class Student {
 
     private Long id;
+    
+    @NotBlank(message = "이름은 필수 입력 항목")
     private String name;
+    
+    @Email(message = "이메일은 @ 형식 필수")
     private String email;
+    
+    @Min(value = 1, message = "나이는 1세 이상만 입력 가능")
     private Integer age;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
