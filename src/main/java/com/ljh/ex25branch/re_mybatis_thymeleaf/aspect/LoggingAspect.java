5702047341c2eb0a8f6e79725729eb76294e11ef
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-10-29 11:24:02 +0900

    Create LoggingAspect.java

---
+++ src/main/java/com/ljh/ex25branch/re_mybatis_thymeleaf/aspect/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.ljh.ex25branch.re_mybatis_thymeleaf.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.ljh.ex25branch.re_mybatis_thymeleaf.service.*.*(..))")
+    public void logBefore(JoinPoint jp){
+        log.info("### 메서드 실행전: {} ###Before###", jp.getSignature());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.ljh.ex25branch.re_mybatis_thymeleaf.service.*.*(..))", returning = "result")
+    public void logAfter(JoinPoint jp, Object result) {
+        log.info("### 메서드\n 실행성공: {}, \n결과: {} ###AfterReturning###", jp.getSignature(), result);
+    }
+}
